1806a1df8c972653190ffc236c7afd372089bdbb
AidaS <ASatiadzhieva@cdtnue.onmicrosoft.com>
2026-07-08 12:12:56 +0200

    info

--- SQL/03_insert_data.sql
+++ SQL/03_insert_data.sql
@@ -5,3 +5,15 @@ insert into benutzer values
 ('Light', 'Yagami'),
 ('Bilbo', 'Baggins'); 
 
+insert into konto (k_id, bez, k_stand) values
+
+(1, 'Gemeinschaftskonto', 4000.00),
+(2, 'John Girokonto', 100000.00),
+(3, 'John Bargeld', 0.00),
+(4, 'SpongeBob Girokonto', 60.00),
+(5, 'SpongeBob Bargeld', 900.00),
+(6, 'Light Girokonto', 100.00),
+(7, 'Light Bargeld', 450.00),
+(8, 'Bilbo Girokonto', 35.75),
+(9, 'Bilbo Bargeld', 450.00);
+
