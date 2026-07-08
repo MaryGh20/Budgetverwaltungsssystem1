2f8cc49623e9b2b71af492dc515efc09d7ba84d6
KiraL <rekiraly@gmail.com>
2026-07-08 16:11:17 +0200

    ohe

--- SQL/01_create_database.sql
+++ SQL/01_create_database.sql
@@ -1,33 +1,38 @@
--- schauen wir alle transaktionen mit name und konto
+-- schauen wir alle kontozustand für alle konto
 SELECT
-    --b.b_id,
     b.Vorname,
     b.Nachname,
-    k.bez,
-    k.k_stand,
-    t.summe,
-    t.date_zeit
-    --t.Beschreibung
-FROM Benutzer b
-JOIN Benutzer_Konto bk 
-    ON b.b_id = bk.bk_id
+    k.bez 'Konto name',
+    k.k_stand 'Kontostand'
+FROM benutzer_konto bk
+JOIN Benutzer b 
+    ON b.b_id = bk.ben_id
 JOIN Konto k 
-    ON bk.bk_id = k.k_id
-JOIN Transaktion t 
-    ON k.k_id = t.konto_id;
+    ON bk.kon_id = k.k_id
+    
+
+--ein Gemeinsamer Kontostand
+select sum(k.k_stand) 'ein Gemeinsamer Kontostand'  from konto k;
+
 
 -- überprüfen wir Transaktion + Kategorie
-
+set dateformat ydm;
 SELECT
-    t.trans_id,
-    t.summe,
     t.date_zeit,
-    k.Name AS Kategorie
+    a.art_trans_bez,
+    k.kat_name Kategorie,
+    t.summe
 FROM Transaktion t
 JOIN art_transfer a
-    ON t.trans_id = a.at_id
+    ON t.art_id = a.at_id
 JOIN Kategorie k
-    ON k.kat_id = a.at_id;
+    ON k.kat_id = t.kategorie_id
+where month(t.date_zeit) = '06' --and year(t.date_zeit)='2026'
+--Where t.date_zeit between '2026.06.01' and '2026.07.01'  
+/*where t.date_zeit >= '2026-06-01'
+and t.date_zeit < '2026-07-01'
+order by t.date_zeit;*/
+
 
 -- überprufen Einnahme und Ausgabe
 
