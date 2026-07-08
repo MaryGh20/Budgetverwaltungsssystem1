dd8a6dc2d8bd67ef617b7c06d5a48f909d4ae381
Maryam <igor.mghafouri@cdt.onmicrosoft.com>
2026-07-08 10:02:57 +0200

    tabele

---
+++ shema.projekt1.sql
@@ -0,0 +1,39 @@
+
+
+create table Benutzer (
+B_id int identity primary key,
+vorname varchar(15),
+nachname varchar(15)
+);
+
+
+create table Konto (
+K_id int primary key,
+Bezeichnung varchar(30),
+Kontostand Decimal (15,2)
+);
+
+create table Benutzer_konto (
+Benutzer_id int,
+Konto_id int,
+Primary key (Benutzer_id, Konto_id), 
+Foreign key (Benutzer_id) references Benutzer (B_id),
+Foreign Key (Konto_id) references Konto(K_id)
+);
+
+create table transaktion (
+trans_id int,
+Date_Zeit Datetime,
+Summe Decimal(15,2),
+Konto_id int,
+art_id int,
+Primary key (trans_id), 
+Foreign key (Konto_id) references Konto (K_id),
+Foreign Key (Art_id) references Art_Transfer(a_id)
+);
+
+create table Art_Transfer (
+A_id int primary key,
+Art_bez varchar(30)
+);
+
